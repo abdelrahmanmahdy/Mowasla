b828d081d279d9d8c1672d7c83111b4194e13ece
abdelrahmanmahdy <abdelrahman.tare.mahdy@gmail.com>
2017-07-30 20:50:26 +0200

    Backendless integrated (Login & Register)

--- InternshipApp/app/src/main/java/com/overcoffee/internshipapp/SplashActivity.java
+++ InternshipApp/app/src/main/java/com/overcoffee/internshipapp/SplashActivity.java
@@ -5,12 +5,16 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.backendless.Backendless;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Backendless.initApp(this,Defaults.APPLICATION_ID,Defaults.API_KEY);
 
         new CountDownTimer(1500,500) {
             @Override
